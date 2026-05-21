5cdd788a1181744b5b70240221b8ab4a2f84fd1c
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-05-21 07:04:49 +0300

    Подключение к бд

---
+++ Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УП_01._01_Gusakov323
+{
+    internal class Core
+    {
+        public static ReadWriteNoSleepEntities Context = new ReadWriteNoSleepEntities();
+
+        public static void ResetContext()
+        {
+            Context = new ReadWriteNoSleepEntities();
+        }
+    }
+}
