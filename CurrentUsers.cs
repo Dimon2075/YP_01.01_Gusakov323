6ddd5b23542c56fc311a60e80fcec195afa46510
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-05-21 07:12:47 +0300

    Создана папка с пустыми страницами и вспомогающий класс

---
+++ CurrentUsers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УП_01._01_Gusakov323
+{
+    public static class CurrentUser
+    {
+        public static Users User { get; set; }
+        public static bool IsAdmin => User?.RoleID == 3;
+        public static bool IsAuthor => User?.RoleID == 2;
+        public static bool IsFrozen => User?.IsFrozen == true;
+
+    }
+}
